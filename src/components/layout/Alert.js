be8f0dd194a550e28a3b09a34b3582aed0f187be
ivandi1980 <ivandi.djoh@gmail.com>
2019-09-18 18:22:35 +0800

    modified all codes

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,17 +1,18 @@
-import React, { useContext } from 'react'
-import AlertContext from '../../context/alert/alertContext'
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
 const Alert = () => {
-    const alertContext = useContext(AlertContext)
-    const { alert } = alertContext
+  const alertContext = useContext(AlertContext);
 
-    return (
-        alert !== null && (
-            <div className = {`alert alert-${alert.type}`}>
-                <i className = 'fas fa-info-circle' /> {alert.msg}
-            </div>
-        )
+  const { alert } = alertContext;
+
+  return (
+    alert !== null && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className='fas fa-info-circle' /> {alert.msg}
+      </div>
     )
-}
+  );
+};
 
-export default Alert
+export default Alert;
